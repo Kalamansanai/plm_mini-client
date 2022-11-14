177090b3e005bdcb3786c52745383734939b3264
anguaive <angurg98@gmail.com>
2022-11-14 12:58:17 +0100

    Cleanup & add generated api client

---
+++ src/api_client/apis/index.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './DetectorsApi';
+export * from './EventsApi';
+export * from './JobsApi';
+export * from './LinesApi';
+export * from './LocationsApi';
+export * from './OPUsApi';
+export * from './SitesApi';
+export * from './StationsApi';
+export * from './TasksApi';
