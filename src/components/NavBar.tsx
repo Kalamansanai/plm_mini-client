f57a173d0d796660e5a741be407e7f1d2ed50648
anguaive <angurg98@gmail.com>
2022-12-02 15:59:06 +0100

    Update Navbar structure
    I didn't see the need for the previously used swipeable menu on the left
    side, because the current frontend structure is more "tightly knit"
    while previously it was more like a collection of independent screens.
    
    - User button: pops up a window showing information about the current user
    and the session. If the user is a superuser, they can also navigate to
    the admin panel and the audit log from here.
    
    - Organization: link to the CompanyHierarchy screen.
    
    - Dashboard: link to the Dashboard screen. If previously there was a
      Station selected, the link will redirect to that Station's dashboard
      (if a Location was selected, it will open that Location). If no
      Station was selected, the user will be directed to the
      CompanyHierarchy screen.
    
    - Data: link to the Data screen. The Data screen will contain aggregate
      views of all Detectors, Jobs, Tasks, etc. in a tabular format.
    
    - About: link to the About screen.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,23 +1,17 @@
-import { useState } from "react";
-
-import ClearIcon from "@mui/icons-material/Clear";
-import MenuIcon from "@mui/icons-material/Menu";
-import { styled, SwipeableDrawer, useTheme } from "@mui/material";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import FactoryIcon from "@mui/icons-material/Factory";
+import InfoIcon from "@mui/icons-material/Info";
+import ListAltIcon from "@mui/icons-material/ListAlt";
+import { Box, Button, Divider, styled, Tooltip } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
 import IconButton from "@mui/material/IconButton";
-import List from "@mui/material/List";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 
 const Offset = styled("div")(({ theme }) => theme.mixins.toolbar);
 
 export default function NavBar() {
-    const theme = useTheme();
-    const [drawerOpen, setDrawerOpen] = useState(false);
-    const [currentRouteIndex, setCurrentRouteIndex] = useState(0);
-
-    const toggleDrawer = () => {};
-
     return (
         <>
             <AppBar
@@ -25,31 +19,44 @@ export default function NavBar() {
                 sx={{ zIndex: (theme) => theme.zIndex.drawer + 1, bgcolor: "primary.dark" }}
             >
                 <Toolbar>
-                    <IconButton color="inherit" onClick={toggleDrawer}>
-                        {drawerOpen ? (
-                            <ClearIcon fontSize="large" />
-                        ) : (
-                            <MenuIcon fontSize="large" />
-                        )}
-                    </IconButton>
-                    <Typography component="div" variant="h6" sx={{ pl: 2 }}>
+                    <Typography component="div" variant="h5">
                         Production Line Monitoring
                     </Typography>
+                    <Box display="flex" gap={2} sx={{ ml: "auto" }}>
+                        <Button size="large" sx={{ color: "white", fontSize: "1.2em" }}>
+                            <div>Superuser</div>
+                            <AccountCircleIcon fontSize="large" sx={{ ml: 2 }} />
+                        </Button>
+                        <Divider
+                            flexItem
+                            orientation="vertical"
+                            variant="middle"
+                            sx={{ bgcolor: "white", mx: 1 }}
+                        />
+                        <Tooltip title="Organization">
+                            <IconButton sx={{ color: "white" }}>
+                                <FactoryIcon fontSize="large" />
+                            </IconButton>
+                        </Tooltip>
+                        <Tooltip title="Dashboard">
+                            <IconButton sx={{ color: "white" }}>
+                                <DashboardIcon fontSize="large" />
+                            </IconButton>
+                        </Tooltip>
+                        <Tooltip title="Data">
+                            <IconButton sx={{ color: "white" }}>
+                                <ListAltIcon fontSize="large" />
+                            </IconButton>
+                        </Tooltip>
+                        <Tooltip title="About">
+                            <IconButton sx={{ color: "white" }}>
+                                <InfoIcon fontSize="large" />
+                            </IconButton>
+                        </Tooltip>
+                    </Box>
                 </Toolbar>
             </AppBar>
             <Offset />
-            <SwipeableDrawer
-                variant="temporary"
-                open={drawerOpen}
-                onOpen={() => setDrawerOpen(true)}
-                onClose={() => setDrawerOpen(false)}
-                anchor="left"
-            >
-                <Offset />
-                <List
-                    sx={{ position: "relative", width: theme.drawerWidth, height: "100%" }}
-                ></List>
-            </SwipeableDrawer>
         </>
     );
 }
