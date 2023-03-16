a75171e990f6d71ea1d73a9ea4b21bc91f915629
Bór Milán <milanbor40@gmail.com>
2023-03-16 15:07:57 +0100

    popup on top

--- src/components/_screens/dashboard/Dashboard.tsx
+++ src/components/_screens/dashboard/Dashboard.tsx
@@ -2,11 +2,12 @@ import { backend, config as apiConfig, DetailedError } from "api";
 import { LocationsApi, ResponseError } from "api_client";
 import axios from "axios";
 import { plainToClass } from "class-transformer";
-import React, { useEffect, useState } from "react";
-import { Params, useLoaderData, useRouteLoaderData } from "react-router-dom";
+import React, { MutableRefObject, useEffect, useRef, useState } from "react";
+import { Link, Params, useLoaderData, useNavigate, useRouteLoaderData } from "react-router-dom";
 import { Location, OngoingTask, parseDetectorState, Station } from "types";
 
-import { Box, Grid, Typography, useMediaQuery, useTheme } from "@mui/material";
+import ClearIcon from "@mui/icons-material/Clear";
+import { Box, Button, Grid, IconButton, Typography, useMediaQuery, useTheme } from "@mui/material";
 
 import DetectionControls from "./DetectionControls";
 import NextStepGuide from "./NextStepGuide";
@@ -109,12 +110,30 @@ export default function Dashboard() {
     const isBelowXl = useMediaQuery(theme.breakpoints.down("xl"));
 
     const task = location.ongoingTask;
-    const steps = task?.steps;
     const instance = task?.ongoingInstance;
+
+    const detState = useRef(false);
+    const isSuccessful = useRef(true);
+    const detectionSuccessful = useRef(false);
+
+    if (instance) {
+        detState.current = true;
+    }
+
+    if (detState.current) {
+        detectionSuccessful.current = instance?.currentOrderNumRemainingSteps === undefined;
+    }
+
+    if (!instance) {
+        detState.current = false;
+    }
 
     return (
         <Grid container spacing={2} height="100%">
             <Grid display="flex" flexDirection="column" gap={2} item xs={12} xl={9}>
+                {detectionSuccessful.current ? (
+                    <EndDetectionAlert location_id={location.id} />
+                ) : null}
                 <Stream
                     playing={playing}
                     detector={location.detector}
@@ -160,6 +179,35 @@ export default function Dashboard() {
                     </Box>
                 </Grid>
             ) : null}
+        </Grid>
+    );
+}
+
+type Props = {
+    location_id: number;
+};
+
+function EndDetectionAlert({ location_id }: Props) {
+    const navigate = useNavigate();
+    return (
+        <Grid
+            display="flex"
+            flexDirection="row"
+            justifyContent="center"
+            sx={{ width: "100%", borderRadius: 3, minHeight: 50, backgroundColor: "white" }}
+            alignItems="center"
+        >
+            <Typography sx={{ ml: 2, mr: 1 }}>
+                Detection Task finished ! The timstamps of the events are available on the instances
+                panel !
+            </Typography>
+            <Button
+                variant="outlined"
+                sx={{ ml: 1, mr: 2 }}
+                onClick={() => navigate(`../prev_instances/${location_id}`)}
+            >
+                View
+            </Button>
         </Grid>
     );
 }
